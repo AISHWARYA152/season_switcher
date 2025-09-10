f46ba700c4fd1a2d93b9b0d93430226651960e50
Aishwarya Chinagundi <nagathanshreeshail@gmail.com>
2025-09-10 08:46:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let seasonSmallImageElement = document.getElementById("seasonSmallImage");
+
+let springSmallImage = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+
+function changeSeasonToSpring() {
+    seasonSmallImageElement.src = springSmallImage;
+}
+
+let summerSmallImage = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+
+function changeSeasonToSummer() {
+    seasonSmallImageElement.src = summerSmallImage;
+}
+
+let autumnSmallImage = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+
+function changeSeasonToAutumn() {
+    seasonSmallImageElement.src = autumnSmallImage;
+}
+
+let winterSmallImage = "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+
+function changeSeasonToWinter() {
+    seasonSmallImageElement.src = winterSmallImage;
+}
